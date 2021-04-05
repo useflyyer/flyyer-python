4e4b553a3c0a7b2200cb782540fef6c475deae2f
Franco Méndez <f@francomendez.com>
2021-04-05 12:48:32 -0400

    feat: add flayyer ai support

--- flayyer/flayyer.py
+++ flayyer/flayyer.py
@@ -54,6 +54,70 @@ class Flayyer:
         return self.href()
 
 
+class FlayyerAI:
+    def __init__(
+        self,
+        project: str,
+        path: str,
+        secret: Optional[str] = None,
+        strategy: Optional[str] = None,
+        variables: Optional[Mapping[Any, Any]] = None,
+        meta: Optional[FlayyerMeta] = None,
+    ):
+        self.project = project
+        self.path = path if path else "/"
+        self.secret = secret
+        self.strategy = strategy
+        self.variables = variables if variables else {}
+        self.meta = meta if meta else {}
+        if (strategy and strategy.lower() != "hmac" and strategy.lower() != "jwt"):
+          raise Exception("Invalid `strategy`. Valid options are `HMAC` or `JWT`.")
+        if (strategy and not secret):
+          raise Exception("Missing `secret`. You can find it on your project in Advanced settings.")
+
+    def querystring(self, ignoreV=False) -> str:
+        if (not ignoreV):
+            defaults = {
+                "__v": self.meta.get(
+                    "v", str(int(time()))
+                ),  # This forces crawlers to refresh the image
+                "__id": self.meta.get("id"),
+                "_w": self.meta.get("width"),
+                "_h": self.meta.get("height"),
+                "_res": self.meta.get("resolution"),
+                "_ua": self.meta.get("agent"),
+            }
+            aux = to_query({**defaults, **self.variables}).split("&")
+            aux.sort()
+            return "&".join(aux)
+        else:
+            defaults = {
+                "__id": self.meta.get("id"),
+                "_w": self.meta.get("width"),
+                "_h": self.meta.get("height"),
+                "_res": self.meta.get("resolution"),
+                "_ua": self.meta.get("agent"),
+            }
+        aux = to_query({**defaults, **self.variables}).split("&")
+        aux.sort()
+        return "&".join(aux)
+
+    def sign(self) -> str:
+        if (self.strategy == None):
+            return "_"
+        elif (self.strategy and self.strategy.lower() == "hmac"):
+            pass
+        elif (self.strategy and self.strategy.lower() == "jwt"):
+            pass
+
+    def href(self) -> str:
+        query = self.querystring()
+        signature = self.sign()
+        return f"https://flayyer.ai/v2/{self.project}/{signature}/{query}{self.path}"
+
+    def __str__(self):
+        return self.href()
+
 # From https://stackoverflow.com/a/43347067/3416691
 # Alternative: https://stackoverflow.com/a/4014164/3416691
 def to_query(params: Mapping[Any, Any]) -> str:
