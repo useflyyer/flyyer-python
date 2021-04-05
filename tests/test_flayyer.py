b7d04835528a61f84d3294e19e016e2703d4863f
Franco Méndez <f@francomendez.com>
2021-04-05 18:38:42 -0400

    test: add signatures test

--- tests/test_flayyer.py
+++ tests/test_flayyer.py
@@ -55,17 +55,7 @@ def test_complex_stringify():
     result = to_query(data)
     assert unquote(result) == "a[aa]=bar&a[ab]=foo&b[0][c]=foo&b[1][c]=bar"
 
-def test_url_encoding_ai():
-    flayyer = FlayyerAI(
-        project="project",
-        path="/path/to/product",
-        variables={"title": "Hello world!"},
-    )
-    href = flayyer.href()
-    assert match("https:\/\/flayyer.ai\/v2\/project\/_\/__v=\d+&title=Hello\+world%21\/path\/to\/product", href) != None
-
-
-def test_meta_parameters_ai():
+def test_ai_meta_parameters():
     flayyer = FlayyerAI(
         project="project",
         path="/path/to/product",
@@ -80,3 +70,58 @@ def test_meta_parameters_ai():
     )
     href = flayyer.href()
     assert match("https:\/\/flayyer.ai\/v2\/project\/_\/__id=dev\+forgot\+to\+slugify&__v=\d+&_h=200&_ua=whatsapp&_w=100&title=title\/path\/to\/product", href) != None
+
+def test_ai_encode_url_happy_path():
+    flayyer = FlayyerAI(
+        project="project",
+        path="/path/to/product",
+        variables={"title": "Hello world!"},
+    )
+    href = flayyer.href()
+    assert match("https:\/\/flayyer.ai\/v2\/project\/_\/__v=\d+&title=Hello\+world%21\/path\/to\/product", href) != None
+
+def test_ai_encode_url_default_values():
+    flayyer = FlayyerAI(
+        project="project",
+    )
+    href = flayyer.href()
+    assert match("https:\/\/flayyer.ai\/v2\/project\/_\/__v=\d+\/", href) != None
+
+def test_ai_encode_url_with_path_missing_slash_at_start():
+    flayyer = FlayyerAI(
+        project="project",
+        path="path/to/product",
+        variables={"title": "Hello world!"},
+    )
+    href = flayyer.href()
+    assert match("https:\/\/flayyer.ai\/v2\/project\/_\/__v=\d+&title=Hello\+world%21\/path\/to\/product", href) != None
+
+def test_ai_encode_url_with_signature_hmac():
+    flayyer = FlayyerAI(
+        project="project",
+        path="/collections/col",
+        secret="sg1j0HVy9bsMihJqa8Qwu8ZYgCYHG0tx",
+        strategy="HMAC",
+        meta=FlayyerMeta(
+            id="dev forgot to slugify",
+            width="100",
+            height=200,
+        ),
+    )
+    href = flayyer.href()
+    assert match("https:\/\/flayyer.ai\/v2\/project\/e8771c307e66652f\/__id=dev\+forgot\+to\+slugify&__v=\d+&_h=200&_w=100\/collections\/col\/?", href) != None
+
+def test_ai_encode_url_with_signature_jwt():
+    flayyer = FlayyerAI(
+        project="project",
+        path="/collections/col",
+        secret="sg1j0HVy9bsMihJqa8Qwu8ZYgCYHG0tx",
+        strategy="JWT",
+        meta=FlayyerMeta(
+            id="dev forgot to slugify",
+            width="100",
+            height=200,
+        ),
+    )
+    href = flayyer.href()
+    assert match("https:\/\/flayyer.ai\/v2\/project\/jwt-eyJhbGciOiJIUzI1NiJ9.eyJpZCI6ImRldiBmb3Jnb3QgdG8gc2x1Z2lmeSIsIndpZHRoIjoiMTAwIiwiaGVpZ2h0IjoyMDB9.0KpbuqbwJyNVNqPOJO_LzvqOXCQK51_WbGEm3pFyY9s\/?\?__v=\d+", href) != None
